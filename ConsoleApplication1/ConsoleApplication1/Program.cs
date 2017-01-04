8c934892493f92d1dbc9f62aa574d8a3f591e0aa
W0289350 <W0289350@nscc.ca>
2017-01-04 12:21:56 -0400

    WRITE SOMETHING HERE BRANDON

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (Console.KeyAvailable)
+                {
+                    Console.WriteLine("HIT DAT I KEY PLZ");
+                    ConsoleKeyInfo fartKey = Console.ReadKey(true);
+                    if (fartKey.Key == ConsoleKey.X)
+                    {
+                        Console.WriteLine("AW SHIT YOU TYPED X");
+                    }
+                    else
+                    {
+                        Console.WriteLine("YOU DID NOT TYPE X");
+                    }
+                }
+                //This will only run due to the key available if statement. Otherwise it will wait each time.
+                Console.WriteLine("Listening...");
+            }           
+        }
+    }
+}
